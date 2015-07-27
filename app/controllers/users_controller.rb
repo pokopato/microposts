ed624bdb3ba98c6e2faba1fb4210d35b87fff774
pokopato <patora20140808@gmail.com>
2015-07-27 06:09:19 +0000

    add update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
-  before_action :login_required
   
   def new
     @user = User.new
   end
   
   def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
     @user = User.find(params[:id])
   end
   
@@ -17,6 +20,18 @@ class UsersController < ApplicationController
       redirect_to user_path(@user)
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.assign_attributes(user_params)
+    if @user.save
+      flash[:success] = "更新完了です！"
+      redirect_to @user
+    else
+      flash[:danger] = "更新できませんでした。"
+      render 'edit'
     end
   end
   
